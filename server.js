07c1ecc77514e80ac3171f073c8f5484fe44b2a2
MidStrLK <stepma@list.ru>
2017-11-23 17:26:04 +0300

    настройки для опеншифта

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 const port           = require('./node/shared/settings').port;
+const server_port           = process.env.OPENSHIFT_NODEJS_PORT || port || 8080;
+const server_ip_address     = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./node/routes')(app, {});
 
-app.listen(port, () => {
-    console.log('We are live on ' + port);
+app.listen(server_port, server_ip_address, () => {
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
